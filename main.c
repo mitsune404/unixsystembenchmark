7a710947e0d4d9e616bfcf56a20ea1f823e30f96
Aman <amacoool75@gmail.com>
2021-04-22 22:20:21 +0530

    Testing some prime number algorithm and CPU speed

--- main.c
+++ main.c
@@ -1,7 +1,43 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void main() {
+int main()
+{
+  int n, i = 3, count, c;
 
-  printf("HIII");
+  printf("Enter the number of prime numbers required :  ");
+  scanf("%d", &n);
+  printf("\n");
+  printf(system("date +%T"));
 
+  if (n >= 1)
+  {
+    printf("\n%dth prime number is :  ", n);
+    if (n == 1)
+    {
+      printf("2 ");
+    }
+  }
+
+  // iteration for n prime numbers
+  // i is the number to be checked in each iteration starting from 3
+  for (count = 2; count <= n; i++)
+  {
+    // iteration to check c is prime or not
+    for (c = 2; c < i; c++)
+    {
+      if (i % c == 0)
+        break;
+    }
+
+    if (c == i) // c is prime
+    {
+      //printf("%d ", i);
+      //printf("%d ", c);
+      count++; // increment the count of prime numbers
+    }
+  }
+  printf("\n%d\n", i);
+
+  printf(system("date +%T"));
+  return 0;
 }
